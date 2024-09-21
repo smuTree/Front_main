94ef3b67fee581871f78e43d0242f19a8de102cb
SAILGO <121847777+SA-ILGO@users.noreply.github.com>
2024-09-21 12:33:21 +0900

    [FEAT] weaponMade 데이터 전송

--- src/components/Player.js
+++ src/components/Player.js
@@ -77,35 +77,14 @@ export const Player = () => {
             raycaster.current.setFromCamera(mouse.current, camera);
             const intersects = raycaster.current.intersectObjects(scene.children, true);
 
-            // Raycaster 시각화
-            // if (lineRef.current) {
-            //     const rayDirection = raycaster.current.ray.direction.clone().multiplyScalar(100); // 광선 길이
-            //     const endPoint = raycaster.current.ray.origin.clone().add(rayDirection);
-            //     lineRef.current.geometry.setFromPoints([raycaster.current.ray.origin, endPoint]);
-            // }
-
             // 물체 강조
             if (intersects.length > 0) {
                 const clickedObject = intersects[0].object;
 
-                // 테두리 강조 업데이트
-                // if (edgeRef.current) {
-                //     edgeRef.current.visible = false; // 기본적으로 숨김
-                //     const geometry = new EdgesGeometry(clickedObject.geometry);
-                //     const material = new LineBasicMaterial({ color: 0xff0000, linewidth: 2 });
-                //     edgeRef.current.geometry.dispose(); // 이전 geometry 삭제
-                //     edgeRef.current.geometry = geometry;
-                //     edgeRef.current.material = material;
-                //     edgeRef.current.visible = true;
-                // }
-
                 if (event.shiftKey && clickedObject.name === "crystalCube") {
-                    console.log("Attempting to remove");
+                    //console.log("Attempting to remove");
 
                     const { x, y, z } = intersects[0].point;
-        
-                    //const roundX = Math.round(x);
-                    //const roundZ = Math.round(z);
 
                     removeCube(x, 0.3, z); // 반올림된 좌표로 삭제
                     setCubesRemoved(cubesRemoved + 1);
@@ -113,12 +92,12 @@ export const Player = () => {
 
                 if (intersects.length > 0) {
                     const clickedObject = intersects[0].object;
-                    console.log('Clicked object:', clickedObject.name || clickedObject);
+                    //console.log('Clicked object:', clickedObject.name || clickedObject);
                 }
                 
                 if (clickedObject.name === "furnace001" || clickedObject.name === "furnace" || clickedObject.name === "furnace1") {
                     if(cubesRemoved > 0){
-                        console.log("Clicked furnace");
+                        //console.log("Clicked furnace");
                         meltCrystal();
                         setCubesRemoved(cubesRemoved - 1);
                         setCrystalMelted(crystalMelted + 1);
@@ -126,8 +105,40 @@ export const Player = () => {
                 } 
                 if (clickedObject.name === "anvil") {
                     if(crystalMelted > 0){
-                        setCrystalMelted(crystalMelted - 1);
-                        setWeaponMade(weaponMade + 1);
+                            setCrystalMelted(crystalMelted - 1);
+                            setWeaponMade(weaponMade + 1);
+                            
+
+                            const submitPost = (e) => {
+    
+                                console.log("SCORE: " + weaponMade);
+                                const post = {
+                                    weaponMade: weaponMade + 1,
+                                };
+
+                                fetch(`http://localhost:3000/InGame/post`, {
+                                    method: "POST",
+                                    headers: {
+                                        'Content-Type': 'application/json',
+                                        'Accept': 'application/json'    
+                                    },
+                                    body: JSON.stringify(post),
+                                })
+                                .then((res) => {
+                                    if (!res.ok) {
+                                        throw new Error("Network response was not ok");
+                                    }
+                                    return res.json(); 
+                                })
+                                .then((data) => {
+                                    console.log(data);
+                                })
+                                .catch((error) => {
+                                    console.error("Error:", error);
+                                });
+                            };
+
+                            submitPost();
                     }
 
                 } 
@@ -138,16 +149,6 @@ export const Player = () => {
         return () => window.removeEventListener('click', handleClick);
     }, [camera, scene, addCube, meltCrystal, removeCube, cubesRemoved, setCubesRemoved, crystalMelted, setCrystalMelted, weaponMade, setWeaponMade]);
 
-    return (
-        <>
-            <mesh ref={ref}>
-                {/* Player's physical body (optional) */}
-            </mesh>
-
-
-            
-        </>
-    );
 };
 
 
