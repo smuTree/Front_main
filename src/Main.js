2c95e76a7beb3a8fe139076a431dbc82fef32361
yeonee <leenayeon@sookmyung.ac.kr>
2024-09-21 16:40:33 +0900

    css 수정

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Html, useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber'; // Import Canvas
+import { Canvas, useFrame } from '@react-three/fiber'; // Import Canvas and useFrame
 import { useNavigate } from 'react-router-dom';
 import './Main.module.css';
 
 export default function Main() {
   const { scene } = useGLTF('/planet.gltf'); // useGLTF 훅 사용
+  const planetRef = useRef(); // useRef 훅으로 오브젝트 참조 생성
   const navigate = useNavigate();
 
   function clickLogin() {
     console.log("play now button clicked");
-    navigate('/NickName');
+    navigate('/login');
   }
 
   function clickRanking() {
     navigate('/Ranking');
+  }
+
+  // GLTF 모델을 포함한 회전하는 컴포넌트
+  function RotatingPlanet() {
+    useFrame(() => {
+      if (planetRef.current) {
+        planetRef.current.rotation.y += 0.01; // Y축을 기준으로 회전 (공전 효과)
+      }
+    });
+
+    return (
+      <group ref={planetRef} position={[0, 0, 0]} scale={[0.55, 0.55, 0.55]}>
+        <primitive object={scene} />
+      </group>
+    );
   }
 
   return (
@@ -24,15 +40,13 @@ export default function Main() {
       <pointLight color={'#B778FF'} position={[10, 10, 10]} intensity={3} />
       <spotLight color={'#B778FF'} position={[0, 10, 0]} angle={0.15} penumbra={1} intensity={1} />
 
-      <group position={[0, 0, 0]} scale={[0.9, 0.9, 0.9]}>
-        <primitive object={scene} />
-      </group>
+      {/* 회전하는 행성 컴포넌트 */}
+      <RotatingPlanet />
 
       {/* HTML elements */}
       <Html center>
         <div className="centered-container">
           <h1>CosmicMiner</h1>
-          {/* <button className="button" onClick={clickGame}>Start</button> */}
           <button className="button" onClick={clickLogin}>Start</button>
           <button className="button" onClick={clickRanking}>Ranking</button>
         </div>
--- src/NickNmae.js
+++ src/NickNmae.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Html } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Html, useGLTF } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { useNavigate } from 'react-router-dom';
+
 import './NickName.css'; // CSS 파일 import
 
 export default function NickName() {
     const navigate = useNavigate();
-    function createNickname(){
+    const { scene } = useGLTF('/planet.gltf'); // useGLTF 훅 사용
+    const planetRef = useRef();
+    const BACKEND_IP = process.env.REACT_APP_BACKEND_IP;
+
+    function createNickname() {
         const nickname = document.getElementById("Nickname").value;
         console.log(nickname);
-        navigate('/login');
+        navigate('/InGame');
+    }
+
+    // GLTF 모델을 포함한 회전하는 컴포넌트
+    function RotatingPlanet() {
+        useFrame(() => {
+            if (planetRef.current) {
+                planetRef.current.rotation.y += 0.01; // Y축을 기준으로 회전 (공전 효과)
+            }
+        });
+
+        return (
+            <group ref={planetRef} position={[0, 0, 0]} scale={[0.55, 0.55, 0.55]}>
+                <primitive object={scene} />
+            </group>
+        );
     }
 
     return (
         <Canvas>
+            <ambientLight color={'#ffffff'} intensity={3} />
+            <pointLight color={'#B778FF'} position={[10, 10, 10]} intensity={3} />
+            <spotLight color={'#B778FF'} position={[0, 10, 0]} angle={0.15} penumbra={1} intensity={1} />
+
+            {/* 공전하는 오브젝트 */}
+            <RotatingPlanet />
+
             <Html center>
-                <div className="container">
+                <div className="centered-container">
                     <span>닉네임을 입력하세요</span>
                     <input 
                         id='Nickname' 
